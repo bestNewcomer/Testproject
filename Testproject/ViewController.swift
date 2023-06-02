05f09ef1c5602dc1dbdc49fc0c70474c24cd4f7d
bestNewcomer <NewcomerRUS@yandex.ru>
2023-06-02 15:19:50 +0300

    Выполнено практическое задание к Спринту № 3 Когорта 9.

--- Testproject/ViewController.swift
+++ Testproject/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private weak var logTextView: UITextView!
+    
+    @IBOutlet private weak var counterLabel: UILabel!
+    var counter: Int = 0
+    
+    @IBAction private func pressPlusButton(_ sender: Any) {
+        counter += 1
+        let date = Date()
+        let dateFormatter = DateFormatter ()
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+        let logDate = dateFormatter.string(from: date)
+        counterLabel.text = "Значение счетчика: \(counter)"
+        logTextView.text += "\n\(logDate): значение изменено на +1"
+    }
+    
+    @IBAction private func pressMinusButton(_ sender: Any) {
+        counter -= 1
+        let date = Date()
+        let dateFormatter = DateFormatter ()
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+        let logDate = dateFormatter.string(from: date)
+        if counter < 0 {
+            counter = 0
+            counterLabel.text = "Значение счетчика: \(counter)"
+            logTextView.text += "\n\(logDate): попытка уменьшить значение счётчика ниже 0"
+        }else{
+            counterLabel.text = "Значение счетчика: \(counter)"
+            logTextView.text += "\n\(logDate): значение изменено на -1"
+        }
+    }
+    
+    @IBAction private func pressClearButton(_ sender: Any) {
+        counter = 0
+        let date = Date()
+        let dateFormatter = DateFormatter ()
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+        let logDate = dateFormatter.string(from: date)
+        counterLabel.text = "Значение счетчика: \(counter)"
+        logTextView.text += "\n\(logDate): значение сброшено"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
 
